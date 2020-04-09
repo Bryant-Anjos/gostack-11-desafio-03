687827c888215d634651df1227d14519010f3ebd
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2020-04-08 21:31:11 -0300

    Desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [title, setTitle] = useState('')
+  const [url, setUrl] = useState('')
+  const [techs, setTechs] = useState('')
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data))
+  }, [])
+
+  async function handleAddRepository(e) {
+    e.preventDefault()
+
+    const response = await api.post('repositories', {
+      title,
+      url,
+      techs: techs.split(',').map(tech => tech.trim()),
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    const repositoriesUpdated = repositories.filter(
+      repository => repository.id !== id
+    )
+    setRepositories([...repositoriesUpdated])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <h1>Adicionar repositório</h1>
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="url"
+          placeholder="link do repositório"
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Tecnologias (separadas por vírgula)"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          required
+        />
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
